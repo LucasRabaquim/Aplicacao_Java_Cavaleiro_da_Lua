86d491ad45c1d60cf91d5bf7db5320fa378f720f
Lucas Daniel da Costa Rabaquim <98958822+LucasRabaquim@users.noreply.github.com>
2022-09-23 09:28:18 -0300

    iniciando intent implicita

--- app/src/main/java/com/example/cavaleiro_da_lua/diferencas_Activity.java
+++ app/src/main/java/com/example/cavaleiro_da_lua/diferencas_Activity.java
@@ -2,7 +2,9 @@ package com.example.cavaleiro_da_lua;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 public class diferencas_Activity extends AppCompatActivity {
 
@@ -10,5 +12,8 @@ public class diferencas_Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_diferencas);
+    }
+    public void btn_hq1(View view){
+      Uri site = Uri.parse("")
     }
 }
